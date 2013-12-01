de428935b2eb22cb037fd6181fbfc8a46a49ccd9
Jeff Cohen <jeff@starterleague.com>
2013-12-01 15:55:44 -0600

    Prepare for class

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Prices are in cents
+product_data = [
+  { name: "Waffle Maker", price: '2499' },
+  { name: "Stapler", price: '999' },
+  { name: "iPad", price: '49900' },
+  { name: "TV", price: '69500' },
+  { name: "Chair", price: '11995' },
+  { name: "Table", price: '29900' },
+  { name: "Bookshelf", price: '7900' },
+  { name: "Laptop", price: '89900' }
+]
+
+Product.destroy_all if Rails.env.development?
+
+product_data.each do |item|
+  product = Product.new(name: item.name, price: item.price)
+  product.save
+end
+
+puts "#{Product.count} products."
